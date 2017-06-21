b75c692317a1d165e6a63d4c9a62ccea4b72d8f9
Hannah Nicklas <hannahnick27@gmail.com>
2017-06-20 20:55:56 -0700

    working on python

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
+@app.route("/", methods=['POST'])
+def noentry():
+    username = request.form['username']
+    password = request.form['password']
+    verify_password = request.form['verify']
+
+    if username == '':
+        error = ""
+
+
 @app.route("/")
 def index():
     return render_template('signup_page.html',
